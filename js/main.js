f4443cf16242d869ce36a5e83b3635ad29aeac89
Melissa Lajoie <mlajoie@echidna.ca>
2019-11-20 20:23:17 -0500

    update wording, js speed and switch to using em

--- js/main.js
+++ js/main.js
@@ -6,6 +6,6 @@
 	var arrow = document.getElementById("arrow");
 
 	arrow.onclick = function() {
-	  TweenLite.to(window, 1, {scrollTo:"#about"});
+	  TweenLite.to(window, 0.65, {scrollTo:"#about"});
 	}
 })();
